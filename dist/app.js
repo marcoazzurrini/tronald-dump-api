4f5d13cd5e00157aeccc3281e8be9e22a2e34401
marcoazzurrini <marco.azzurrini.art@gmail.com>
2019-09-25 12:12:52 +0200

    first commit

---
+++ dist/app.js
@@ -0,0 +1,22 @@
+// ASSIGN DOM ELEMENTS TO VARIABLES
+const form = document.getElementById('main-form');
+const textInput = document.getElementById('text');
+const output = document.getElementById('output');
+
+// FORM LISTENS FOR SUBMIT
+form.addEventListener('submit', getQuote);
+
+// FUNCTION GET QUOTE
+function getQuote(e) {
+    e.preventDefault();
+
+    fetch(`https://api.tronalddump.io/random/quote`, {
+        headers: {
+            "accept": "application/hal+json"
+        },
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+
+}
